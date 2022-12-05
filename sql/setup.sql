9863f1e92f8eadad1fb6ef5bc9b3b2127c7ac0bd
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-12-05 09:18:13 -0800

    refactored sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,18 +2,18 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS secrets CASCADE;
+DROP TABLE IF EXISTS secrets;
 
 CREATE TABLE users (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR UNIQUE,
+    password_hash VARCHAR NOT NULL
 );
 
 CREATE TABLE secrets (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    description VARCHAR,
     created_at TIMESTAMP WITH TIME ZONE
 DEFAULT CURRENT_TIMESTAMP
 );
@@ -21,3 +21,4 @@ DEFAULT CURRENT_TIMESTAMP
 INSERT INTO secrets (title, description)
 VALUES
 ('secret secret, very hush hush', 'aliens created Idaho, government does not want you to know');
+('Keith Richards should be dead', 'PUPPIES ARE SACRIFICED TO KEEP HIM ALIVE.');
